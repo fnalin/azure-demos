abf1574f9168c91b8663815dc833c3aa1168c31f
fabiano nalin <fnalin@msn.com>
2020-01-28 16:01:47 -0300

    Add Azure Table Storage Demo

---
+++ table-storage/TableStorage/Model.cs
@@ -0,0 +1,17 @@
+using Microsoft.Azure.Cosmos.Table;
+
+namespace TableStorage
+{
+    public class CustomerEntity : TableEntity
+{
+    public CustomerEntity(string lastName, string firstName)
+    {
+        this.PartitionKey = lastName;
+        this.RowKey = firstName;
+    }
+    public CustomerEntity() { }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+}
---
+++ table-storage/TableStorage/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace TableStorage
+{
+    class Program
+    {
+        private static CloudTable _table;
+
+        static async Task Main(string[] args)
+        {
+            var account = CloudStorageAccount.Parse("STRING DE CONEXÃO DO STORAGE");
+            var client = account.CreateCloudTableClient();
+
+            _table = client.GetTableReference("people");
+
+            await _table.CreateIfNotExistsAsync();
+
+            var customer1 = new CustomerEntity("Haynes", "Jodie");
+            customer1.Email = "jodie@contoso.com";
+            customer1.PhoneNumber = "425-555-0101";
+
+            AddCustomer(customer1);
+
+            GetData();
+
+            Console.WriteLine("FIM");
+            Console.ReadLine();
+
+
+
+        }
+
+        private static void GetData()
+        {
+            var condition = TableQuery.GenerateFilterCondition(
+                            "PartitionKey", QueryComparisons.Equal, "Haynes");
+            var query = new TableQuery<CustomerEntity>().Where(condition);
+            foreach (CustomerEntity entity in _table.ExecuteQuery(query))
+            {
+                Console.WriteLine($"{entity.RowKey} {entity.PartitionKey} [Email: {entity.Email} | Phone: {entity.PhoneNumber}]");
+            }
+
+        }
+
+        private static void AddCustomer(CustomerEntity customer1)
+        {
+            var insertOperation = TableOperation.Insert(customer1);
+            _table.Execute(insertOperation);
+
+        }
+    }
+}
