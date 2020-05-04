3c4a02b4ef6baf4c4abdc5b3d2e3f9ef005e4b2c
fabiano nalin <fnalin@msn.com>
2020-05-04 15:28:47 -0300

    Add Query via REST

---
+++ azure-cognitive-search/04 - query via rest/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AzureSearchRestAPI
+{
+    public class Cliente
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ azure-cognitive-search/04 - query via rest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+namespace AzureSearchRestAPI
+{
+    class Program
+    {
+
+        private static HttpClient httpClient = new HttpClient();
+        static void Main(string[] args)
+        {
+             // Baseado em https://docs.microsoft.com/en-us/azure/search/search-get-started-postman
+
+            var configuration =
+                new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                .Build();
+
+            var key = configuration["SearchServiceKey"];
+            httpClient.BaseAddress = new Uri(configuration["SearchServiceURL"]);
+            httpClient.DefaultRequestHeaders.Add("ContentType", "application/json");
+            httpClient.DefaultRequestHeaders.Add("api-key", key);
+
+            var indexName = "cadcli-index";
+            //var data = RunQuery(indexName, "*&%24count=true&%24orderby=dateCreated%20desc").Result;
+            var data = RunQuery(indexName, "*&%24top=10&%24orderby=dateCreated%20desc").Result;
+
+            Console.WriteLine(data);
+
+            Console.WriteLine("FIM");
+
+        }
+
+        public static async Task<string> RunQuery(string indexName, string query = "*")
+        {
+            var indices = await httpClient.GetAsync($"{indexName}/docs?api-version=2019-05-06&search={query}");
+            string responseBodyAsText = await indices.Content.ReadAsStringAsync();
+
+            if (indices.StatusCode != HttpStatusCode.OK) return null;
+
+            return responseBodyAsText;
+        }
+
+    }
+}
